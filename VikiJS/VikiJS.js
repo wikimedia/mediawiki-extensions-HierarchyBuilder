7386fc415c0afeba7c2418fa022171b6570294db
Ji, Jason Y <jji@mitre.org>
2014-02-13 15:33:11 -0800

    Debug attempt

--- VikiJS/VikiJS.js
+++ VikiJS/VikiJS.js
@@ -47,10 +47,10 @@ window.VikiJS = {
 	ImagePath: null,
 	Zoompos: 1, // to store values for zoom scale
 	
-	drawGraph: function(pageTitles, fiscalYear, graphDiv,
-		detailsDiv, imagePath, personNames, initialWidth, initialHeight) {
-
+	drawGraph: function(pageTitles, fiscalYear, graphDiv, detailsDiv, imagePath, personNames, initialWidth, initialHeight) {
+		console.log("drawGraph");
 		pageTitles = eval("("+pageTitles+")");
+		console.log("pageTitles: "+pageTitles);
 		self.log("pageTitles: "+pageTitles);
 		VikiJS.GraphDiv = graphDiv;
 		VikiJS.DetailsDiv = detailsDiv;
