749a2fddf68a0b0bc51c90a0fffe517cddec69d6
Ji, Jason Y <jji@mitre.org>
2014-05-08 16:21:33 -0400

    We seem to have gotten a bit behind in ProjectGraph version numbering, so let's just call this one version 1.6.0.

--- ProjectGraph/ProjectGraph.php
+++ ProjectGraph/ProjectGraph.php
@@ -37,7 +37,7 @@ if (version_compare($wgVersion, '1.21', 'lt')) {
 
 $wgExtensionCredits['parserhook'][] = array (
 	'name' => 'ProjectGraph',
-	'version' => '1.5.1',
+	'version' => '1.6.0',
 	'author' => array("Cindy Cicalese", "Jason Ji", "Austin Vecchio"),
 	'descriptionmsg' => 'projectgraph-desc'
 );
