99a98e49ad3e014ea2ab324a22f7f8f36429bb78
Keven Fobis <forbes@gestalt-ed.localdomain>
2014-07-09 17:57:06 -0400

    Changed .js code for ResourceLoader thingy. Removed extraneous old code.

--- ImportBibTeX/ImportBibTeX.js
+++ ImportBibTeX/ImportBibTeX.js
@@ -1,18 +1,10 @@
-if (!String.prototype.trim) {
-  String.prototype.trim = function () {
-    return this.replace(/^\s+|\s+$/g, '');
-  };
-}
 
-function importBibTeX(div, api_url, xml, overwrite, injurytype) {
+window.importBibTeX = function (div, api_url, xml, overwrite, injurytype) {
 
 	var div_progress = div + "_progress";
 	var div_duplicates = div + "_duplicates";
 	var div_errors = div + "_errors";
 	var div_details = div + "_details";
-
-	//var records = jQuery(jQuery.parseXML(xml)).find("record");
-	//var records = xml.split("@");
 
     var b = new BibtexParser();
     b.setInput(xml);
@@ -40,13 +32,13 @@ records.length + " (<span id='" + div_duplicates + "'>0</span>" +
 	importSet(div_progress, div_duplicates, div_errors, div_details, api_url, records, overwrite, injurytype);
 }
 
-function importSet(div_progress, div_duplicates, div_errors, div_details,
+window.importSet = function (div_progress, div_duplicates, div_errors, div_details,
 	api_url, records, overwrite, injurytype) {
 	importOne(div_progress, div_duplicates, div_errors, div_details, api_url,
 		records, overwrite, 0, 0, 0, injurytype);
 }
 
-function importOne(div_progress, div_duplicates, div_errors, div_details,
+window.importOne = function (div_progress, div_duplicates, div_errors, div_details,
 	api_url, records, overwrite, index, duplicates, errors, injurytype) {
 	if (index >= records.length) return;
 	var action = "add_bibtex";
@@ -97,124 +89,8 @@ function importOne(div_progress, div_duplicates, div_errors, div_details,
 	});
 }
 
-function getFields(tempString) {
-    var fields = {};
 
-    //Grab bibtexType
-    var index = tempString.indexOf("{");
-    var bibtexType = tempString.substring(0, index);
-    index++;
-    tempString = tempString.substring(index, tempString.length);
-    //document.write(bibtexType+ "<bR>");
-    
-    //Grab second metadata
-    index = tempString.indexOf(",");
-    var secondMetadata = tempString.substring(0, index);
-    index++;
-    tempString = tempString.substring(index, tempString.length);
-    //document.write(secondMetadata+ "<bR>");
-    
-    index = tempString.indexOf("=");
-                                           
-    while(index != -1) {
-        var header = tempString.substring(0, index-1).trim();
-    
-        index++;
-        //document.write("header: \"" + header + "\"" + "<br>");
-        tempString = tempString.substring(index).trim();
-      	
-        if(tempString.charAt(0) == "{") {
-            var index2 = tempString.indexOf("}");
-            var value = tempString.substring(1, index2).trim();
-            //document.write("value: \"" + value + "\"" + "<br>");
-            index = index2 + 2;
-            fields[header] = value;
-        } else {
-            var index2 = tempString.indexOf(",");
-            if(index2 == -1) {
-        	    index2 = tempString.indexOf("}");
-            }
-        	var value = tempString.substring(0, index2).trim();
-        	//document.write("value: \"" + value + "\"" + "<br>");
-        	index = ++index2;
-        	fields[header] = value;
-    	}
-      	
-		tempString = tempString.substring(index).trim();
-    	index = tempString.indexOf("=");       
-    }
-    
-	if(fields["type"] == null) {
-    	fields["type"] = bibtexType;
-    }
-    
-	//document.write(index + "<br>");
-	//document.write("fields[type]: " + fields["type"] + "<br>");
-    
-	return fields;
-    
-}
-/*
-function getFields(record) {
-	var tags = [
-		"ref-type",
-		"title",
-		"tertiary-title",
-		"full-title",
-		"abstract",
-		"call-num",
-		"isbn",
-		"language",
-		"notes",
-		"pages",
-		"publisher",
-		"pub-location",
-		"volume",
-		"date",
-		"year",
-		"electronic-resource-num",
-		"url"
-		];
-	var fields = {};
-	var i;
-	for (i in tags) {
-		var tag = tags[i];
-		var xtag = tag;
-		fields[xtag] = "";
-		var elements = record.getElementsByTagName(tag);
-		var j;
-		for (j = 0; j < elements.length; j++) {
-			var element = elements[j];
-			if (element.textContent.length > 0) {
-				if (fields[xtag].length == 0) {
-					fields[xtag] = element.textContent;
-				} else {
-					fields[xtag] += ";" + element.textContent;
-				}
-			}
-		}
-	}
-	fields.authors = "";
-	elements = record.getElementsByTagName("author");
-	for (j = 0; j < elements.length; j++) {
-		var element = elements[j];
-		if (element.textContent.length > 0) {
-			fields.authors += "<author>" + element.textContent + "</author>";
-		}
-	}
-	fields.keywords = "";
-	elements = record.getElementsByTagName("keyword");
-	for (j = 0; j < elements.length; j++) {
-		var element = elements[j];
-		if (element.textContent.length > 0) {
-			fields.keywords += "<keyword>" + element.textContent + "</keyword>";
-		}
-	}
-	return fields;
-}
-*/
-
-function BibtexParser() {
+window.BibtexParser = function() {
   this.pos = 0;
   this.input = "";
   
