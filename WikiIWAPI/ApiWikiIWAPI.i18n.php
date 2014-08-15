ebbada5c7e82d21c4720b32d41f2d94d594df31d
Alex Lyte <alyte@mitre.org>
2014-08-15 15:17:14 -0400

    Changed ext name in desc file

--- WikiIWAPI/ApiWikiIWAPI.i18n.php
+++ WikiIWAPI/ApiWikiIWAPI.i18n.php
@@ -1,5 +1,5 @@
 <?php
 $messages = array();
 $messages['en'] = array(
-        'iwlinks-desc' => "This extension implements a call to the InterWiki API module to provide a list of all wikis on the interwiki table. For more information, see api.php",
+        'interwiki-api' => "This extension implements a call to the InterWiki API module to provide a list of all wikis on the interwiki table. For more information, see api.php",
 );
