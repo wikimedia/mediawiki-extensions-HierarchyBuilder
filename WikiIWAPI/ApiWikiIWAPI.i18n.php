44b864b3eb2d6ff0c0f0517498d1e7dc5ea72393
Alex Lyte <alyte@mitre.org>
2014-08-15 15:13:15 -0400

    Edited the IWAPI description

--- WikiIWAPI/ApiWikiIWAPI.i18n.php
+++ WikiIWAPI/ApiWikiIWAPI.i18n.php
@@ -1,5 +1,5 @@
 <?php
 $messages = array();
 $messages['en'] = array(
-        'iwlinks-desc' => "This extension implements an iwlinks API module to provide a list of all wikis on the iwlinks table. For more information, see api.php",
+        'iwlinks-desc' => "This extension implements a call to the InterWiki API module to provide a list of all wikis on the interwiki table. For more information, see api.php",
 );
