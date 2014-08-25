6d9c6a035975c88ebc58267575c5127313cbef80
Alex Lyte <alyte@mitre.org>
2014-08-24 21:00:02 -0400

    Structured User data

--- WikiUserTool/src/getWikis.js
+++ WikiUserTool/src/getWikis.js
@@ -44,38 +44,128 @@ function propogateStats(vikiObject){
 // We need to extract the users from these. So, for each wiki, make an array of users. 
 // Each of those users will have an array of events. 
 
+// add all the logs to an array
+var allLogs = [];
+vikiObject.activeWikis.forEach(function(w){
+	w.logs.forEach(function(l){
+		l.wiki = w.wikiTitle;
+		var date = new Date(l.timestamp);
+		l.hour = date.getHours();
+		l.day = date.getDay();
+		l.month = date.getMonth();
+		allLogs.push(l);
+	})
+})
+vikiObject.objAry = populate(allLogs, 'user', 'day');
+
+// the two keys to filter on
+function populate(ary, key1, key2){
+	var lAry = [];
+	var result = {};
+	ary.forEach(function(l){
+		var val1 = l[key1];
+		var val2 = l[key2];		
+		function containsObj(element){
+			return (element['row'] === val1 && element['col'] === val2);
+		}
+		var obj = lAry.filter(containsObj);
+//		console.log(obj)
+		// if it's in the array, add 1 to the representative
+		if(obj.length < 1){
+		  lAry.push(
+			{
+			  'row' : val1,
+			  'col' : val2,
+			  'val' : 1
+			}
+		  )
+		} else {
+		  obj[0].val++;
+		}
+		// otherwise, add it to the array
+	})	
+return lAry;
+}
+
+
+
+
+
 	vikiObject.activeWikis.forEach(function(w){
 		var wiki = w;
-		w.logs.forEach(function(l){
-			wiki.users = [];
+		wiki.users = [];
+		var currentDate = new Date();
+		var currentMonth = currentDate.getMonth();
+		var currentDay = currentDate.getDay();
+		var currentHour = currentDate.getHours();
+		wiki.logs.forEach(function(l){
+			var date = new Date(l.timestamp);
+			l.hour = date.getHours();
+			l.day = date.getDay();
+			l.month = date.getMonth();
+		});
+
+// parse through all the logs. for each log, create an element with that user and row, and a val equal to 1; 
+// pop that element into an array. 
+// if that element is already in the array, 
+
+
+
+		// array of objects of the form {'row' : username, 'col' : day, 'val' : logCount}
+		wiki.heatArray = [];
+		wiki.users.forEach(function(u){
+		  // for each user, create an object for every unique date. push it to the array
+		  wiki.logs.forEach(function(l){
+		    // if the log is for this user
+		    if(l.user === u){
+			// if the heatmap array doesn't already contain that day, add it. Otherwise, ++ that day
+
+		    }
+		  })		
+		})
+
+		wiki.logs.forEach(function(l){
 			var user = l.user;
-			if(wiki.users[user] !== undefined){
-				wiki.users.user.push(l)
-			} else {
-				wiki.users[user] = [];
+			wiki.udata = [];
+			// if the user is not in the array, add it to it
+			if(wiki.users.indexOf(user) === -1){
+				wiki.users.push(user);
+//				wiki.udata.push(
+//					{ 'name' : user,
+//					  'row' : countTypes(wiki, 'day'),
+//					}
+//				)				
 			}
+		
 		})
 	})
 	console.log(vikiObject)
-
 }
 
-function getWikiLogs(vikiObject){
-	var lastWiki = vikiObject.activeWikis[vikiObject.activeWikis.length-1].wikiTitle;
+//we want to create an array of month, user pairs. So, for each user, we want to create an array of 
+// {user : 1, day : 1, val : 0}, {user : 1, day : 2, val : 0}
+// so we can use a map function to loop through every value in the logs and for each element 
 
-	vikiObject.activeWikis.forEach(function(d){
-		d.wUrl = d.apiURL + "?action=query&list=logevents&format=json&lelimit=500"
-		d.logs = [];
-		if(d.wikiTitle !== lastWiki){		
-			recursiveQuery(d);
-		} else {
-			finalRecursiveQuery(d);
+// return an array 
+function countTypes(wiki, col){
+        var monthArray = [];		
+	wiki.monthData = {};
+	// loop through all the logs on this wiki
+	wiki.logs.forEach(function(l){
+		// if we are counting by day, 
+		if(col === 'day'){
+//		  for(var m=1; m<33; m++){
+//		    if(l.day === day){
+		      wiki.monthData[l.day] !== undefined ? wiki.monthData[l.day]++ : wiki.monthData[l.day] = 1;
+//		    }		  
+//		  }
+//		  monthArray.forEach(function(day){})
 		}
 	})
-	
-	propogateStats(vikiObject)
-
+//return monthData;
 }
+
+
 
 function populateWikiLogs(vikiObject){
 	vikiObject.i = 0;
@@ -179,123 +269,6 @@ function incrementalRecursiveQuery(vikiObject){
 
 
 
-function finalRecursiveQuery(wiki){
-	jQuery.ajax({
-		url: wiki.wUrl,
-		dataType: 'jsonp',
-		async: false,
-		success: function (data, textStatus, jqXHR) {
-			if(data.hasOwnProperty("query") && data.query.logevents.length > 0){
-				data.query.logevents.forEach(function(l){wiki.logs.push(l)})
-				var lastLogDate = data.query.logevents[data.query.logevents.length-1].timestamp;
-				var milliFromNow = 	Date.parse(Date()) - Date.parse(lastLogDate)
-	
-				// if the last log was less than a year old, then
-				if(milliFromNow < logSize){
-					// if there are any more log events, then
-					if (data.hasOwnProperty("query-continue")){        
-					  // if it has a 'lecontinue', query that
-					  if(data["query-continue"].logevents.hasOwnProperty("lestart")){
-
-						  var qCont = data['query-continue'].logevents.lestart;
-						  var newQuery = wiki.wUrl.split("&lestart=")[0] + "&lestart=" + qCont;
-
-//						  console.log("New lecstart Query");
-//						  console.log(newQuery)
-
-						  wiki.wUrl = newQuery;
-						  recursiveQuery(wiki);
-
-					  } else if(data["query-continue"].logevents.hasOwnProperty("lecontinue")){
-						  var qCont = data['query-continue'].logevents.lecontinue;
-						  var newQuery = wiki.wUrl.split("&lecontinue=")[0] + "&lecontinue=" + qCont;
-
-//						  console.log("New lecontinue Query");
-//						  console.log(newQuery)
-
-						  wiki.wUrl = newQuery;
-						  recursiveQuery(wiki);
-
-					  } else {
-//							console.log("No leStuff")
-//							console.log(data)
-					  }
-					
-					} else {
-//					  console.log("No Query Continue");
-//					  console.log(data);
-					}		  
-				}
-			}
-		},
-		error: function (jqXHR, textStatus, errorThrown) {
-//		   error(textStatus);
-		}
-	}); 	
-}
-
-
-
-// For a given wiki, query the logs using the wUrl. 
-// Push the log objects onto the wiki's log array
-// If the timestamp of the last log event is less than a year ago and there is a query-continue, append the query-continue value to the URL.
-// Call recursiveQuery again. 
-function recursiveQuery(wiki){
-	jQuery.ajax({
-		url: wiki.wUrl,
-		dataType: 'jsonp',
-		async: false,
-		success: function (data, textStatus, jqXHR) {
-			if(data.hasOwnProperty("query") && data.query.logevents.length > 0){
-				data.query.logevents.forEach(function(l){wiki.logs.push(l)})
-				var lastLogDate = data.query.logevents[data.query.logevents.length-1].timestamp;
-				var milliFromNow = 	Date.parse(Date()) - Date.parse(lastLogDate)
-	
-				// if the last log was less than a year old, then
-				if(milliFromNow < logSize){
-					// if there are any more log events, then
-					if (data.hasOwnProperty("query-continue")){        
-					  // if it has a 'lecontinue', query that
-					  if(data["query-continue"].logevents.hasOwnProperty("lestart")){
-
-						  var qCont = data['query-continue'].logevents.lestart;
-						  var newQuery = wiki.wUrl.split("&lestart=")[0] + "&lestart=" + qCont;
-
-//						  console.log("New lecstart Query");
-//						  console.log(newQuery)
-
-						  wiki.wUrl = newQuery;
-						  recursiveQuery(wiki);
-
-					  } else if(data["query-continue"].logevents.hasOwnProperty("lecontinue")){
-						  var qCont = data['query-continue'].logevents.lecontinue;
-						  var newQuery = wiki.wUrl.split("&lecontinue=")[0] + "&lecontinue=" + qCont;
-
-//						  console.log("New lecontinue Query");
-//						  console.log(newQuery)
-
-						  wiki.wUrl = newQuery;
-						  recursiveQuery(wiki);
-
-					  } else {
-//							console.log("No leStuff")
-//							console.log(data)
-					  }
-					
-					} else {
-//					  console.log("No Query Continue");
-//					  console.log(data);
-					}		  
-				}
-			}
-		},
-		error: function (jqXHR, textStatus, errorThrown) {
-//		   error(textStatus);
-		}
-	}); 	
-}
-
-
 
 window.getIWTable = function(vikiObject){
 	
@@ -357,6 +330,48 @@ window.getIWTable = function(vikiObject){
 
 
 
+
+
+// Creates a hierarchy out of the data
+ function genJSON(csvData, groups) {
+
+    var genGroups = function(data) {
+      return _.map(data, function(element, index) {
+        return { name : index, children : element };
+      });
+    };
+
+    var nest = function(node, curIndex) {
+      if (curIndex === 0) {
+        node.children = genGroups(_.groupBy(csvData, groups[0]));
+        _.each(node.children, function (child) {
+          nest(child, curIndex + 1);
+        });
+      }
+      else {
+        if (curIndex < groups.length) {
+          node.children = genGroups(
+            _.groupBy(node.children, groups[curIndex])
+          );
+          _.each(node.children, function (child) {
+            nest(child, curIndex + 1);
+          });
+        }
+      }
+      return node;
+    };
+    return nest({}, 0);
+  }
+
+
+
+function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+}
 
 
 
