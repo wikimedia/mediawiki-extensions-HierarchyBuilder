037a3f3f2783913330f8376d22d815c8a567a7b4
Kevin Ji <kji@mitre.org>
2014-10-08 00:48:05 -0400

    Removed all the non-HierarchyBuilder related extensions.

--- Reading/Reading.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-	$input = $('#synopsis');
-	$input.wikiEditor();
-	$input.wikiEditor('addModule',
-		jQuery.wikiEditor.modules.toolbar.config.getDefaultConfig());
-});
--- WikiIWAPI/ApiWikiIWAPI.i18n.php
+++
@@ -1,5 +0,0 @@
-<?php
-$messages = array();
-$messages['en'] = array(
-        'interwiki-api' => "This extension implements a call to the InterWiki API module to provide a list of all wikis on the interwiki table. For more information, see api.php",
-);
